4542b27f108283ae0adcaadcdcc2df84805c16aa
BrunoReis18 <bruno180298@gmail.com>
2023-08-29 06:17:14 +0100

    removed telescope filebrowser associated keybidnigns and settings

--- init.lua
+++ init.lua
@@ -419,8 +419,6 @@ vim.keymap.set('n', '<C-h>','<C-w><C-l>')
 -- Enable telescope fzf native, if installed
 pcall(require('telescope').load_extension, 'fzf')
 
--- Enable telescope file_broswer, if installed 
-pcall(require('telescope').load_extension, 'file_browser')
 
 -- See `:help telescope.builtin`
 vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
@@ -433,7 +431,6 @@ vim.keymap.set('n', '<leader>/', function()
   })
 end, { desc = '[/] Fuzzily search in current buffer' })
 
-vim.keymap.set('n','<leader>fb',require('telescope').extensions.file_browser.file_browser)
 vim.keymap.set('n', '<leader>gf', require('telescope.builtin').git_files, { desc = 'Search [G]it [F]iles' })
 vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
 vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
