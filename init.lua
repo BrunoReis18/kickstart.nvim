868e4061391c09ce72a09917ce3382cc4e70b034
BrunoReis18 <bruno180298@gmail.com>
2023-08-30 08:22:10 +0100

    added keybinding to reset hunk staging

--- init.lua
+++ init.lua
@@ -151,6 +151,7 @@ require('lazy').setup({
         vim.keymap.set('n', '<leader>gn', require('gitsigns').next_hunk, { buffer = bufnr, desc = '[G]o to [N]ext Hunk' })
         vim.keymap.set('n', '<leader>ph', require('gitsigns').preview_hunk, { buffer = bufnr, desc = '[P]review [H]unk' })
         vim.keymap.set('n', '<leader>ah', require('gitsigns').stage_hunk, { buffer = bufnr, desc = '[A]dd [H]unk' })
+        vim.keymap.set('n', '<leader>rh', require('gitsigns').reset_hunk, { buffer = bufnr, desc = '[R]eset [H]unk' })
       end,
     },
   },
